92399e69f0c0e41f18bedd17c1497ebc551ff605
leonidas_malachias <129744095+leonidasmal@users.noreply.github.com>
2023-05-23 21:01:09 +0300

    Add files via upload

--- website/change_password.php
+++ website/change_password.php
@@ -1,57 +1,41 @@
 <?php
-include('connect.php');
 session_start();
+include('connect.php');
 
-// Check if the user is logged in
-if (!isset($_SESSION['username'])) {
-  header("Location: login.php");
-  exit;
-}
-
-// Process the form submission
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Retrieve the form data
-  $currentPassword = $_POST['current_password'];
-  $newPassword = $_POST['new_password'];
-  $confirmPassword = $_POST['confirm_password'];
-
-  // Retrieve the user's account information
+if (isset($_POST['submit'])) {
+  $opwd = $_POST['opwd'];
+  $npwd = $_POST['npwd'];
+  $cpwd = $_POST['cpwd'];
   $username = $_SESSION['username'];
-  $sql = "SELECT * FROM users WHERE username = '$username'";
+
+  $sql = "SELECT user_password FROM users WHERE username = '$username'";
   $result = mysqli_query($conn, $sql);
+  $num = mysqli_fetch_array($result);
 
-  if ($result && mysqli_num_rows($result) > 0) {
-    $user = mysqli_fetch_assoc($result);
+  if ($result) {
+    $storedPassword = $num['user_password'];
 
-    // Verify the current password
-    if (password_verify($currentPassword, $user['password'])) {
-      // Check if the new password and confirm password match
-      if ($newPassword === $confirmPassword) {
-        // Hash the new password
-        $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+    if ($opwd === $storedPassword) {
+      if ($npwd === $cpwd) {
+        $updateSql = "UPDATE users SET user_password ='$npwd' WHERE username = '$username'";
+        $updateResult = mysqli_query($conn, $updateSql);
 
-        // Update the user's password in the database
-        $updateSql = "UPDATE users SET password = '$hashedPassword' WHERE username = '$username'";
-        mysqli_query($conn, $updateSql);
-
-        // Redirect to the account page with a success message
-        header("Location: view_account.php?success=1");
-        exit;
+        if ($updateResult) {
+          $_SESSION['msg1'] = "Password Changed Successfully";
+        } else {
+          $_SESSION['msg2'] = "Error updating password: " . mysqli_error($conn);
+        }
       } else {
-        // Passwords do not match
-        $error = "New password and confirm password do not match.";
+        $_SESSION['msg2'] = "New password and confirm password do not match";
       }
     } else {
-      // Current password is incorrect
-      $error = "Current password is incorrect.";
+      $_SESSION['msg2'] = "Current password is incorrect";
     }
   } else {
-    // User not found or error in retrieving data
-    $error = "Error retrieving user account information.";
+    $_SESSION['msg2'] = "Error retrieving user account information: " . mysqli_error($conn);
   }
 }
 ?>
-
 
 
 <!DOCTYPE html>
@@ -61,6 +45,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Change Password</title>
   <link rel="stylesheet" type="text/css" href="user_dash.css">
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+
+    .container {
+      width: 400px;
+      margin: 0 auto;
+    }
+
+    h2 {
+      text-align: center;
+      margin-top: 30px;
+    }
+
+    form {
+      margin-top: 30px;
+    }
+
+    label {
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    input[type="password"] {
+      width: 100%;
+      padding: 8px;
+      margin-bottom: 20px;
+    }
+
+    button[type="submit"] {
+      padding: 10px 20px;
+      background-color: #333;
+      color: #fff;
+      border: none;
+      cursor: pointer;
+    }
+
+    button[type="submit"]:hover {
+      background-color: #000;
+    }
+
+    .error-message {
+      color: red;
+    }
+
+    .success-message {
+      color: green;
+      font-size: 18px;
+    }
+  </style>
 </head>
 <body>
 <header>
@@ -80,24 +115,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <section class="main-content">
   <div class="container">
     <h2>Change Password</h2>
-    <?php if (isset($error)) { ?>
-      <div class="error"><?php echo $error; ?></div>
+    <?php if (isset($_SESSION['msg1'])) { ?>
+      <p class="success-message" style="font-size: 20px;"><?php echo $_SESSION['msg1']; ?></p>
+      <?php unset($_SESSION['msg1']); ?>
+    <?php } ?>
+    <?php if (isset($_SESSION['msg2'])) { ?>
+      <p class="error-message"><?php echo $_SESSION['msg2']; ?></p>
+      <?php unset($_SESSION['msg2']); ?>
     <?php } ?>
     <form method="POST" action="">
       <div>
         <label for="current_password">Current Password:</label>
-        <input type="password" id="current_password" name="current_password" required>
+        <input type="password" id="opwd" name="opwd" required>
       </div>
       <div>
         <label for="new_password">New Password:</label>
-        <input type="password" id="new_password" name="new_password" required>
+        <input type="password" id="npwd" name="npwd" required>
       </div>
       <div>
         <label for="confirm_password">Confirm Password:</label>
-        <input type="password" id="confirm_password" name="confirm_password" required>
+        <input type="password" id="cpwd" name="cpwd" required>
       </div>
       <div>
-        <button type="submit">Change Password</button>
+        <button type="submit" name="submit">Change Password</button>
       </div>
     </form>
   </div>
